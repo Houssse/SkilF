3e1305d11225e06965d52be2d057fee55526baf1
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-04-23 12:44:56 +0300

    refactoring, add bootstrap form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy]
 
   def index
     @articles = Article.all
@@ -9,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -25,12 +25,9 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
-
       if @article.update(article_params)
         flash[:success] = "Object was successfully updated"
         redirect_to @article
@@ -41,7 +38,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to root_path, status: :see_other
@@ -49,6 +45,11 @@ class ArticlesController < ApplicationController
   
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :body)
     end
