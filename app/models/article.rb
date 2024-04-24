bf3c2021e3966bf7734391f7e0579088a6a960a9
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-04-24 14:00:59 +0300

    added time post

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3, maximum: 30}
   validates :body, presence: true, length: {minimum: 3}
+
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
 end
