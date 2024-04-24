6f0085b04132463ae4c9c5c64033cafe33962d37
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-04-24 15:57:07 +0300

    update views gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   resources :articles
 
   root "articles#index"
+
 end
